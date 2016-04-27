595cc186b208810957dd7950bbc0a54b9fda80c5
Lukas Wimmer <lukas.wimmer@fu-berlin.de>
2016-04-27 14:43:43 +0200

    Update singlebands2rasterstack.R

--- singlebands2rasterstack.R
+++ singlebands2rasterstack.R
@@ -1,6 +1,4 @@
-install.packages("rgdal")
 install.packages("raster")
-library(rgdal)
 library(raster)
 
 setwd("/Users/lukaswimmer/Desktop/Wagner_Projekt/02-1973/LM12400771973054FAK03/")
@@ -12,5 +10,3 @@ scene1 <- stack(c("LM12400771973054FAK03_B4.TIF",    # Band 1
                   "LM12400771973054FAK03_B7.TIF"))   # Band 4
 
  writeRaster(scene1, filename="2-73_240-77.tif", overwrite=TRUE)
- 
- #Test change
